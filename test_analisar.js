c31371c493b78a50a654cb79e099ef161edeea70
Gustavo <gustavocapaz06@gmail.com>
2026-05-09 14:03:12 -0300

    chore: move project to root and clean up old files

--- test_analisar.js
+++
@@ -1,9 +0,0 @@
-import { analisarPDFs } from './src/services/iaService.js';
-
-async function test() {
-  const result = await analisarPDFs([
-    { nome: 'doc1.pdf', texto: 'Este é um teste sobre imunologia básica.' }
-  ], 'Imunologia');
-  console.log('RESULTADO FINAL:', result);
-}
-test();
